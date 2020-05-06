678ea73d2b86b6d4687108527c6c211cdce16d2d
quockhanhnv <quockhanhnv0209@gmail.com>
2020-05-06 13:52:29 +0700

    vue router and template login

--- resources/js/components/src/main.js
+++ resources/js/components/src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+import VueRouter from 'vue-router';
+
+// components
+import Login from './components/Auth/Login.vue';
+import Dashboard from './components/HelloWorld.vue';
+
 import App from './App.vue'
+
+
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI, {locale});
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/login', name: 'login', component: Login},
+    {
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard
+    },
+];
+
+const router = new VueRouter({
+    routes
+})
+
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
